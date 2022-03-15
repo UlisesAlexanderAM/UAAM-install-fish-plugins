43363e7304cf7977db7b9e4cdc2ace3f66f1cf27
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-03-15 12:32:26 -0600

    Install plugins from my local/personal repository

--- funtions/install-fish-plugins.fish
+++ funtions/install-fish-plugins.fish
@@ -1,16 +1,17 @@
-function install-fish-plugins -d "Install the fish plugins that I use (from AUR or GH)"
-    if not command -q git > /dev/null
-        echo "Git not installed."
-        echo "Installing git..."
-        sudo pacman -Syu git
-    end
+function install-fish-plugins -d "Install the fish plugins that I use from my local/personal repository"
+    if not rg "uaam-personal" /etc/pacman.conf > /dev/null
+        echo "Repositorio local/personal no configurado"
+        exit
     
-    function __check_if_installed -d "check if the plugin has been already installed from the AUR, and install it if not"
+    end
+
+    function __check_if_installed -d "check if the plugin has been already installed from the my local/personal repository, and install it if not"
         set -l plugin $argv[1]
         if pacman -Qi $plugin > /dev/null
             echo "Plugin $plugin already installed"
         else 
-            paru -Syu $plugin
+            sudo pacman -Syu $plugin
+        
         end
     
     end
@@ -19,34 +20,8 @@ function install-fish-plugins -d "Install the fish plugins that I use (from AUR 
     __check_if_installed fish-fishtape
     __check_if_installed fish-done
     __check_if_installed fish-abbreviation-tips-git
+    __check_if_installed colored_man_pages.fish
+    __check_if_installed spark.fish
 
-    set -l srcdir $HOME/Proyectos
-    if not test -d $srcdir
-        mkdir -pv $srcdir
-    end
-    
-    function __check_project_folder -d "Check if the project folder for the plugin exist"
-        set -l project_name $argv[1]
-        set -l project_folder $srcdir/$project_name
-        if test -d $project_folder
-            git -C $project_folder status
-        else
-            cd $srcdir
-            if test $argv[1] = colored_man_pages.fish
-                git clone https://github.com/PatrickF1/colored_man_pages.fish.git
-            else if test $argv[1] = spark.fish
-                git clone https://github.com/jorgebucaran/spark.fish.git
-                cp -v $project_folder/completions/* $__fish_config_dir/completions/
-            end
-            cp -v $project_folder/funtions/* $__fish_config_dir/funtions/
-        end
-    end
-
-    __check_project_folder colored_man_pages.fish
-    __check_project_folder spark.fish
-
-    functions -e __check_if_installed
-    functions -e __check_project_folder
-    
 end
 
