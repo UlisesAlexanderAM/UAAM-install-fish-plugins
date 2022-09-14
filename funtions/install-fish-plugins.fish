55fb255033f0b5a51a823c9d6a46a247ddd48ecc
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-09-14 16:41:25 -0500

    Use AUR and paru instead of my own repo

--- funtions/install-fish-plugins.fish
+++ funtions/install-fish-plugins.fish
@@ -1,17 +1,13 @@
 function install-fish-plugins -d "Install the fish plugins that I use from my local/personal repository"
-    if not rg "uaam-personal" /etc/pacman.conf > /dev/null
-        echo "Repositorio local/personal no configurado"
-        exit
     
-    end
-
     function __check_if_installed -d "check if the plugin has been already installed from the my local/personal repository, and install it if not"
         set -l plugin $argv[1]
         if pacman -Qi $plugin > /dev/null
             echo "Plugin $plugin already installed"
-        else 
+        else if pacman -Qi $plugin > /dev/null
             sudo pacman -Syu $plugin
-        
+        else 
+            paru -Syu $plugin
         end
     
     end
@@ -20,8 +16,8 @@ function install-fish-plugins -d "Install the fish plugins that I use from my lo
     __check_if_installed fish-fishtape
     __check_if_installed fish-done
     __check_if_installed fish-abbreviation-tips-git
-    __check_if_installed colored_man_pages.fish
-    __check_if_installed spark.fish
+    #__check_if_installed colored_man_pages.fish
+    #__check_if_installed spark.fish
 
 end
 
